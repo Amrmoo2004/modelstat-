9e156469431956dfab1c6e5c95f55cd028dd7851
Amrmoo2004 <amrmohamad489@gmail.com>
2025-08-22 21:13:13 +0300

    Remove unused checkout retrieval functions; streamline checkout service for improved maintainability.

--- src/modules/checkout/checkout.controller.js
+++ src/modules/checkout/checkout.controller.js
@@ -7,7 +7,6 @@ const router = Router();
 router.post('/create',authUser, checkTokenRevoked,checkout.createCheckout);
 router.post('/update/:id', authUser, checkTokenRevoked, checkout.updateafterpayment);
 router.post('/confirm/:id', authUser, checkTokenRevoked, checkout.confirmCheckout);
-router.get('/all', authUser, checkTokenRevoked, checkout.getUserCheckouts);
 
 
 export default router;
